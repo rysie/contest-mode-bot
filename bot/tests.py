dcfd30d20a0f6f05caf504b6a0c87abadbc7dd72
Marcin Macinski <rys@rysie.com>
2023-01-20 12:06:22 +0100

    django test

--- bot/tests.py
+++ bot/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Redditor
 
 # Create your tests here.
+
+
+class RedditorTestCase(TestCase):
+    def setUp(self):
+        Redditor.objects.create(name='test-redditor')
+
+    def test_redditor_exists(self):
+        redditor = Redditor.objects.get(name='test-redditor')
+        self.assertEqual(redditor.name, 'test-redditor')
